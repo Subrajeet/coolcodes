8359243c5d948cac3420b072b39789c5602f3a6a
Subrajeet B <subrajeetbiswas.0898@gmail.com>
2024-11-08 11:24:53 +0530

    Initial Commit

---
+++ suii/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  suii
+//
+//  Created by Subrajeet Biswas on 08/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ suii/suiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  suiiApp.swift
+//  suii
+//
+//  Created by Subrajeet Biswas on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct suiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
